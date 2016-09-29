652d405923b5f2a94d1415f5f29c645255c332ba
Ivan Santos <pragmaticivan@gmail.com>
2016-09-29 10:55:27 -0700

    Add logs support

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
+require 'logger'
 
 class WeDeployApp < Sinatra::Base
+
+  ::Logger.class_eval { alias :write :'<<' }
+  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','access.log')
+  access_logger = ::Logger.new(access_log)
+  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','error.log'),"a+")
+  error_logger.sync = true
+
+  configure do
+    use ::Rack::CommonLogger, access_logger
+  end
+
+  before {
+    env["rack.errors"] =  error_logger
+  }
+
   get '/' do
     erb :home
   end
